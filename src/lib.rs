28ad3f5ec2967dd3f949ce0bfa162d459469d2b6
Lucas Palomo <devlpalomo@gmail.com>
2020-04-24 01:00:36 -0300

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+
+pub mod lib {
+        pub struct ShortText {}
+
+        impl ShortText {
+                pub fn new() -> Self {
+                        ShortText {}
+                }
+
+                pub fn sum(self, n1: i32, n2: i32) -> i32 {
+                        return n1+n2;
+                }
+        }
+}
+
