9f1b11a57723ef4e5127ef10bb6dc072a5fd7ae3
Lucas Palomo <devlpalomo@gmail.com>
2020-04-24 16:15:55 -0300

    struct for ShortText

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
-#[cfg(test)]
+pub struct ShortText {}
 
-pub mod lib {
-        pub struct ShortText {}
+impl ShortText {
+    pub fn new() -> Self {
+        ShortText {}
+    }
 
-        impl ShortText {
-                pub fn new() -> Self {
-                        ShortText {}
-                }
-
-                pub fn sum(self, n1: i32, n2: i32) -> i32 {
-                        return n1+n2;
-                }
-        }
+    pub fn sum(self, n1: i32, n2: i32) -> i32 {
+        return n1 + n2;
+    }
 }
+
 
